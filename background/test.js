ae070979de1afb6c8674eee6095a9877b4d09e91
fgfg163 <fgfg163>
2017-06-14 20:36:54 +0800

    no message

--- background/test.js
+++ background/test.js
@@ -1,11 +1,15 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  chrome.tabs.executeScript(tabId, {
-    code: `
-    let theScript = document.createElement('script');
-    theScript.innerHTML = 'for (var i = 0; i < 5000; i++) { console.log(i); }';
-    document.querySelector('*').appendChild(theScript);
-    `,
-    allFrames: true,
-    runAt: 'document_start',
-  });
+  if (changeInfo.status === 'loading') {
+    if (tab.url.slice(0, 7) === 'http://' || tab.url.slice(0, 8) === 'https://') {
+      chrome.tabs.executeScript(tabId, {
+        code: ';(' + function () {
+          let theScript = document.createElement('script');
+          theScript.innerHTML = 'for (var i = 0; i < 5000; i++) { console.log(i); }';
+          document.querySelector('*').appendChild(theScript);
+        }.toString() + ')();',
+        allFrames: true,
+        runAt: 'document_start',
+      });
+    }
+  }
 });
